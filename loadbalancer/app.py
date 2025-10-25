e071c59d6b2d3b44bc45c7fcf824b9ab59fb0b99
vishalfromvirali <vishalfromvirali@gmail.com>
2025-10-25 20:24:22 +0530

    Fix backend URLs for load balancer

--- loadbalancer/app.py
+++ loadbalancer/app.py
@@ -24,6 +24,6 @@ def loadbalancer():
         except:
             return "server {server_data} have error"
         
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
